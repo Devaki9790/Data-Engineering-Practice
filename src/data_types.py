2d0cfdccf9808096ccf05719c4108a26799c2585
devakimachineni <devakimachineni@gmail.com>
2024-08-14 07:52:40 -0500

    added data types python file

---
+++ src/data_types.py
@@ -0,0 +1,18 @@
+'''int : 1, 3, 5, 7, -3, 0 
+float : 1.0, 0.9, -0.7, 5.6
+string(str) "hello", "0.2", "-98", "true", "False" '''
+# bool : True, False
+# a = True
+# print(type(a)) 
+# b = "hello"
+# c = 3 
+# var = -19.6
+# d = False
+# print(b)
+# print(c)
+# print(var)
+# print(d)
+
+# problem dynamically read the name and say hello name 
+name = input("please enter your name: ")
+print("hello", name )
