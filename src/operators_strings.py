b536d98aa81e94241ee0f6f8987f6747fbc76f3b
devakimachineni <devakimachineni@gmail.com>
2024-08-15 07:58:09 -0500

    added code for operators and strings

--- src/operators_strings.py
+++ src/operators_strings.py
@@ -1,1 +1,29 @@
 # This file will contain operator and string functions examples.
+"""x=5
+y=7
+z=6
+a=-2
+b=1.8
+c=2.5
+d=2.7
+print(x+z)
+print(x*b)
+print(b-c)
+print(z/a)
+print(y%x)
+print(a**4)
+print(a**3)
+print(28//6)
+e=(1+3)/2
+f=4+5*2
+# (),exponents,*,/,+,-
+print(e)
+print(f)"""
+# write a program that reads 2 numbers from user and perform +,-,*,/ on those numbers 
+# output should look like addition=result,subraction=result
+num1=int(input("please enter num1 "))
+num2=int(input("please enter num2 ")) 
+print("addition= ", num1+num2)
+print("subraction= ", num1-num2)
+print("multiplication= ", num1*num2)
+print("division= ", num1/num2)
